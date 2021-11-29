821d5a74dd07c47656357cff9117363208dd5131
Alexandre Cumplido Bou <acumpbo@gmail.com>
2021-11-29 16:41:16 +0100

    Fetch weather infomation

--- apiCall.js
+++ apiCall.js
@@ -1,31 +1,43 @@
-
-
 let header = { headers: { Accept: 'application/json' }};
 let joke;
 let reportJokes =[];
 
 let btnJokes = document.querySelector('#jokes');
 let btnScore = document.querySelectorAll('#score button')
-let jokeTemplate = document.querySelector('p');
-
+let templateJoke = document.querySelector('p');
+let templateWeather = document.querySelector('span')
+let iconWeather = document.querySelector('img');
 
 btnJokes.addEventListener ('click', () => {
     fetch('https://icanhazdadjoke.com/', header)
         .then(response => response.json())
         .then(response => {
-            console.log(response);
             joke = response.joke;
-            jokeTemplate.textContent = `${joke}`;
+            templateJoke.textContent = `${joke}`;
         })
-        .catch(error => console.log(error));
+        .catch(error => templateJoke.textContent = error);
 })
 
-btnScore.forEach( (button) => {
-    button.addEventListener ('click', (event) => {
-        reportJokes.push({
-            jokeText: joke,
-            score: Number(event.target.id),
-            date: new Date().toISOString(),
-        });
+// Prevent double score for same joke
+btnScore.forEach( button => {
+    button.addEventListener ('click', event => {
+        registerJoke(event);
     })
 })
+
+function registerJoke (event) {
+    reportJokes.push({
+        jokeText: joke,
+        score: Number(this.event.target.id), 
+        date: new Date().toISOString(),
+    });
+}
+
+let privateKey = '3a6de1bfb15f6d47dc749e2fc2555d25';
+
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=Barcelona&units=metric&APPID=${privateKey}`)
+    .then(response => response.json())
+    .then(response => {
+        iconWeather.setAttribute('src',`http://openweathermap.org/img/w/${response.weather[0].icon}.png`);
+        templateWeather.textContent = ` ${response.name} ${response.main.temp} celsius`;
+    });
