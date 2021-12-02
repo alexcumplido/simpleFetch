ba26f49035e29d15c90fcef933928d7e6f88f89b
Alexandre Cumplido Bou <acumpbo@gmail.com>
2021-12-02 17:14:21 +0100

    Initial layout

--- src/app.ts
+++ src/app.ts
@@ -10,30 +10,19 @@ let btnScore: NodeListOf<Element> = document.querySelectorAll('#score button')
 let templateJoke: HTMLElement | any = document.querySelector('p');
 let weatherHtml: HTMLElement | any = document.querySelector('span')
 let weatherIcon: HTMLElement | any = document.querySelector('img');
+let shapeBackground :  HTMLElement | any = document.querySelector('section#joke > div');
 
-
-fetchWeather().    
-    then(res => {
-    weatherIcon.setAttribute('src',`http://openweathermap.org/img/w/${res.weather[0].icon}.png`);
-    weatherHtml.textContent = `${res.name} ${res.main.temp} °C`;
-});
 
 async function fetchWeather () {
     let res = await fetch(`${API_WEATHER}${API_WEATHER_KEY}`);
     return await res.json();
 }
 
-btnJokes.addEventListener ('click', () => {
-    if ((Math.floor(Math.random()*2+1)>1)){
-        fetchIcanhaz()
-            .then(res => insertHTML(res.joke))
-            .catch(error => insertHTML(error));
-    }else {
-        fetchCuck()
-            .then(res => insertHTML(res.value))
-            .catch(error => insertHTML(error));
-    }
-})
+fetchWeather().    
+    then(res => {
+    weatherIcon.setAttribute('src',`http://openweathermap.org/img/w/${res.weather[0].icon}.png`);
+    weatherHtml.textContent = `${res.name}, ${res.main.temp} °C`;
+});
 
 async function fetchIcanhaz() {
     const res = await fetch(`${API_DAD}`, {headers:{Accept:'application/json'}});
@@ -49,11 +38,38 @@ function insertHTML (content: any) {
     templateJoke.textContent =  content;
 }
 
-// Prevent double score for same joke
-btnScore.forEach( button => {
-    button.addEventListener ('click', event => {
-        registerJoke(jokeFetched, event);
-    })
+function replaceShape(){
+    switch (Math.floor(Math.random()*3+1)){
+        case 1:
+            shapeBackground.classList.remove("shape3");
+            shapeBackground.classList.remove("shape2");
+            shapeBackground.classList.add('shape1');
+            break;
+        case 2:
+            shapeBackground.classList.remove("shape1");
+            shapeBackground.classList.remove("shape3");
+            shapeBackground.classList.add('shape2');
+            break;
+        case 3: 
+            shapeBackground.classList.remove("shape1");
+            shapeBackground.classList.remove("shape2");
+            shapeBackground.classList.add('shape3');
+            break;
+    }
+}
+    
+btnJokes.addEventListener ('click', () => {
+    if ((Math.floor(Math.random()*2+1)>1)){
+        fetchIcanhaz()
+            .then(res => insertHTML(res.joke))
+            .catch(error => insertHTML(error));
+            replaceShape();
+    }else {
+        fetchCuck()
+            .then(res => insertHTML(res.value))
+            .catch(error => insertHTML(error));
+            replaceShape();
+    }
 })
 
 function registerJoke (jokeFetched: any, event: any) {
@@ -64,3 +80,11 @@ function registerJoke (jokeFetched: any, event: any) {
         date: new Date().toISOString(),
     });
 }
+
+// Prevent double score for same joke
+btnScore.forEach( button => {
+    button.addEventListener ('click', event => {
+        registerJoke(jokeFetched, event);
+    })
+})
+
