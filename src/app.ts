0ae278ac953db059aeb9f70ea0c8384f50fe2854
Alexandre Cumplido Bou <acumpbo@gmail.com>
2021-12-04 13:19:17 +0100

    Folder structure

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 
-const API_WEATHER_K: String = '3a6de1bfb15f6d47dc749e2fc2555d25';
-const API_DAD: String = 'https://icanhazdadjoke.com/';
-const API_CHUCK: String = 'https://api.chucknorris.io/jokes/random';
+const API_WEATHER_K: string = '3a6de1bfb15f6d47dc749e2fc2555d25';
+const API_DAD: string = 'https://icanhazdadjoke.com/';
+const API_CHUCK: string = 'https://api.chucknorris.io/jokes/random';
 
-let jokeFetched: any;
-let reportJokes = [];
-let lat: Number;
-let long: Number;
+let jokeFetched: object | any;
+let reportJokes: any[] = [];
+let lat: number;
+let long: number;
 let weatherHtml: HTMLElement | any = document.querySelector('span')
 let weatherIcon: HTMLElement | any = document.querySelector('img');
 let buttons: NodeListOf<Element> = document.querySelectorAll('button');
@@ -21,7 +21,7 @@ navigator.geolocation.getCurrentPosition((position)=> {
         .then(response => displayWeather(response))
 });
 
-async function fetchWeather (lat: Number, long: Number) {
+async function fetchWeather (lat: number, long: number) {
     const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${API_WEATHER_K}`);
     return await res.json();
 }
